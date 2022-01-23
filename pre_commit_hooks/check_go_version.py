ddb81317d362d75792be94516b6e031614dfb921
xenowits <kumar.abhishek111b@gmail.com>
2022-01-23 23:56:53 +0530

    feat: add hooks for go version check and licence header

---
+++ pre_commit_hooks/check_go_version.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import argparse
+import os
+from typing import Sequence
+from packaging.version import Version   # works for go module versions since go follows semantic versioning
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    print("argv", argv)
+
+    parser.add_argument(
+        '-v',
+        '--version',
+        default='1.17',
+        action="store",
+        required=True,
+        help="expected go version, excluding `patch` version. Ex: 1.17",
+    )
+    args = parser.parse_args(argv)
+
+    # get expected minimum go version
+    reqVersion = args.version
+    if reqVersion is None or not reqVersion.startswith("go"):
+        return 1
+
+    # get go version
+    stream = os.popen('go env GOVERSION')
+    gotVersion = stream.read()
+
+    if Version(gotVersion[2:]) < Version(reqVersion[2:]):
+        print("need to upgrade go version to", reqVersion, "or more")
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ pre_commit_hooks/check_licence_header.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import argparse
+from typing import Any, Sequence
+
+
+LICENCE_HEADER_TEXT = """// Copyright © 2021 Obol Technologies Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License."""
+
+def prepend_licence_header(filename):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(LICENCE_HEADER_TEXT.rstrip('\r\n') + '\n\n' + content)
+
+def isLicenceHeaderPresent(filename: Any) -> bool:
+    with open(filename, 'r') as f:
+        content = f.read()
+        return content.startswith(LICENCE_HEADER_TEXT)
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    args = parser.parse_args(argv)
+
+    return_code = 0
+    for filename in args.filenames:
+        print(f'Filename {filename}', isLicenceHeaderPresent(filename))
+        if not isLicenceHeaderPresent(filename):
+            prepend_licence_header(filename)
+            print(f'Fixing {filename}')
+            return_code = 1
+    return return_code
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())

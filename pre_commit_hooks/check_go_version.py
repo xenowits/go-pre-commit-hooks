6b1db8d178a9ff9c8ae4ec26fbe32e798bd09585
xenowits <kumar.abhishek111b@gmail.com>
2022-01-24 17:38:23 +0530

    add go version as flag

--- pre_commit_hooks/check_go_version.py
+++ pre_commit_hooks/check_go_version.py
@@ -1,17 +1,32 @@
 from __future__ import annotations
 
+import argparse
 import os
-import re
 from typing import Sequence
-
+from packaging.version import Version   # works for go module versions since go follows semantic versioning
 
 def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--version',
+        type=str,
+        help="expected go version, excluding `patch` version. Ex: 1.17",
+    )
+    args = parser.parse_args(argv)
+
+    # get expected minimum go version
+    reqVersion = args.version
+    if not reqVersion.startswith("go"):
+        return 1
+
+    # get go version
     stream = os.popen('go env GOVERSION')
-    output = stream.read()
-    x = re.search("go1.17.*", output)
-    if x: 
-        return 0
-    return 1
+    gotVersion = stream.read()
+
+    if Version(gotVersion[2:]) < Version(reqVersion[2:]):
+        print("need to upgrade go version to", reqVersion, "or more")
+        return 1
+    return 0
 
 
 if __name__ == '__main__':

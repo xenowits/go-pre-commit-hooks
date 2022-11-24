522ead007cb50a03dfad916e20f318943e985a26
xenowits <kumar.abhishek111b@gmail.com>
2022-11-24 14:04:55 +0530

    update check go version hook

--- pre_commit_hooks/check_go_version.py
+++ pre_commit_hooks/check_go_version.py
@@ -12,10 +12,10 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument(
         '-v',
         '--version',
-        default='1.17',
+        default='1.19',
         action="store",
         required=True,
-        help="expected go version, excluding `patch` version. Ex: 1.17",
+        help="expected go version. Ex: 1.19 or 1.19.3",
     )
     args = parser.parse_args(argv)
 
@@ -24,15 +24,16 @@ def main(argv: Sequence[str] | None = None) -> int:
     if reqVersion is None or not reqVersion.startswith("go"):
         return 1
 
-    # get go version
+    # get system go version
     stream = os.popen('go env GOVERSION')
     gotVersion = stream.read()
 
+    # `go env GOVERSION` outputs go version with the `go` prefix. For ex: go1.19.2
     if Version(gotVersion[2:]) < Version(reqVersion[2:]):
         print("need to upgrade go version to", reqVersion, "or more")
         return 1
-    return 0
 
+    return 0
 
 if __name__ == '__main__':
     raise SystemExit(main())
